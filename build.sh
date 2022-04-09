6c1babd095e7f07303562efd9924272747bdbdcc
luke <740710@gmail.com>
2022-04-09 21:06:03 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+# usage
+# aa=$(script_path)
+script_path()
+{
+  CURDIR=$PWD
+  SCRIPT_PATH="${BASH_SOURCE[0]}";
+  while([ -h "${SCRIPT_PATH}" ]); do
+    cd "`dirname "${SCRIPT_PATH}"`"
+    SCRIPT_PATH="$(readlink "`basename "${SCRIPT_PATH}"`")";
+  done
+  cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
+  SCRIPT_PATH="`pwd`";
+  cd $CURDIR
+  echo "$SCRIPT_PATH"
+}
+
+DIR=$PWD
+
+cd $(script_path)
+
+# use Dockerfile in ./
+# other Dockerfile maybe found in docker directory
+docker build docker  -t alarm-clock:build
+
+docker run -it -v$PWD:$PWD alarm-clock:build sh -c "cd $PWD; make"
+
+cd $DIR
